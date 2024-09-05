33398e40240b638c35d750a266f6438e7c6a8dbf
Deflecta <firvelfrivol@gmail.com>
2024-09-05 15:55:29 +0300

    Первый коммит

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Mod-Reposters-Redirector
- 
+# Mod Reposters Redirector (Перенаправление с сайтов-репостеров модов)
+
+## English
+
+A Tampermonkey script designed to redirect users from articles on mod reposter websites to the official mod pages on CurseForge and Modrinth.
+
+Currently, it redirects clicks on mod article titles from the ru Minecraft mod page to the pages that appear as the first search result on Modrinth, based on the mod name from the ru Minecraft article.
+
+Future plans include adding separate buttons for navigation to CurseForge and Modrinth on ru Minecraft, as well as extending support to include not only ru Minecraft but also Minecraft Inside.
+
+## На русском
+
+Скрипт для Tampermonkey, созданный для перенаправления со статей сайтов-репостеров модов на официальные страницы модов на CurseForge и Modrinth.
+
+В данный момент лишь перенаправляет нажатия на названия статей модов на странице модов «ру Майнкрафта» на страницы, оказывающиеся в первом результате поиска на Modrinth по названию мода из статьи на «ру Майнкрафте».
+
+Планируется реализовать вставку отдельных кнопок для перехода на CurseForge и Modrinth на «ру Майнкрафте», а также поддержку не только «ру Майнкрафта», но и Minecraft Inside.
---
+++ mod_reposter_redirector.user.js
@@ -0,0 +1,53 @@
+// ==UserScript==
+// @author          Deflecta
+// @contributionURL https://boosty.to/rushanm
+// @description     Allows searching for mods on CurseForge and Modrinth from reposter websites
+// @description:ru  Позволяет искать моды на CurseForge и Modrinth с сайтов-репостеров
+// @downloadURL     
+// @grant           none
+// @homepageURL     https://rushan.neocities.org/
+// @icon            https://ru-minecraft.ru/templates/rework/assets/images/favicon.ico
+// @license         MIT
+// @match           https://modrinth.com/mods*
+// @match           https://ru-minecraft.ru/mody-minecraft*
+// @name            Mod Reposters Redirector
+// @name:ru         Перенаправление с сайтов-репостеров модов
+// @namespace       http://tampermonkey.net/
+// @supportURL      
+// @updateURL       
+// @version         0.1
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function cleanModName(modName) {
+        return modName.split(" - ")[0].trim();
+    }
+
+    const modLinks = document.querySelectorAll('.news__title a');
+
+    modLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+
+            const modName = cleanModName(link.textContent);
+            const apiUrl = `https://api.modrinth.com/v2/search?query=${encodeURIComponent(modName)}&limit=1`;
+
+            fetch(apiUrl)
+                .then(response => response.json())
+                .then(data => {
+                    if (data.hits && data.hits.length > 0) {
+                        const modUrl = `https://modrinth.com/mod/${data.hits[0].slug}`;
+                        window.location.href = modUrl;
+                    } else {
+                        alert('Не удалось найти мод на Modrinth!');
+                    }
+                })
+                .catch(error => {
+                    console.error('Не удалось получить данные Modrinth:', error);
+                    alert('При поиске мода произошла ошибка.');
+                });
+        });
+    });
+})();
